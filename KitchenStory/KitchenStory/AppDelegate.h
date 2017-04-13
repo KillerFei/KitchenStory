aa4198bb0763efd14b099fb03c34ba22504fb3de
killerFei <18358151209@163.com>
2017-04-13 17:42:05 +0800

    厨房故事纪元开始

---
+++ KitchenStory/KitchenStory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KitchenStory
+//
+//  Created by yuepengfei on 17/4/13.
+//  Copyright © 2017年 fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KitchenStory/KitchenStory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KitchenStory
+//
+//  Created by yuepengfei on 17/4/13.
+//  Copyright © 2017年 fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
